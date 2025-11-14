cf0b809d291ffe9ef669fb8e5a2aadb9d0aa7089
mauro-au <mauricio.a.developer@gmail.com>
2025-11-14 08:45:27 -0300

    Se comienza con proyecto de un crud de usuarios en vanilla javascript usando vite

---
+++ crud-app/src/users/users-app.js
@@ -0,0 +1,13 @@
+import { renderTable } from "./presentation/render-table/render-table";
+import usersStore from "./store/users-store";
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const UsersApp = async (element) => {
+  // element.innerHTML = "Loading...";
+  await usersStore.loadNextPage();
+
+  renderTable(element);
+};
